94b232481efbc3d4d4c57faa831d6a54e4fd5922
DEV2 <dev2@applh.com>
2020-07-15 17:01:07 +0200

    dev2 a créé un TestDev2Controller

---
+++ src/Controller/TestDev2Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestDev2Controller extends AbstractController
+{
+    /**
+     * @Route("/test/dev2", name="test_dev2")
+     */
+    public function index()
+    {
+        return $this->render('test_dev2/index.html.twig', [
+            'controller_name' => 'TestDev2Controller',
+        ]);
+    }
+}
